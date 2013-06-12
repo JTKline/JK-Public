18f972b1aaa3ed0e6b0cb3b482c4f43397126d28
John Kline <ilfrepus@gmail.com>
2013-06-12 11:21:38 -0400

    Test Code Commit 6-12-13

---
+++ Ruby/Test-Ruby-Project/test_class.rb
@@ -0,0 +1,23 @@
+class TestClass
+  attr_accessor :name
+
+  def initialize(name = "John")
+    @name = name
+  end
+
+  def say_hi
+    puts "Hi #{@name}"
+  end
+
+  def say_bye
+    puts "Bye #{@name}"
+  end
+end
+
+aName = TestClass.new
+aName.say_hi
+aName.say_bye
+
+aName.name = "David"
+aName.say_hi
+aName.say_bye
